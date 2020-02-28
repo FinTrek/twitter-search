31b68bab59951939735e546097dd5b9b699b13f8
dannovikov <dannovikov100@gmail.com>
2020-02-28 02:41:38 -0500

    fixed more formatting issues

--- datacollection.py
+++ datacollection.py
@@ -6,31 +6,36 @@ while True:
 	fileCount+=1
 	print('Collecting data for coronavirus... ', end='', flush=True)
 	coronavirus.generateData('coronavirus', fileCount)
-	print('Done.\nSleeping for 15 minutes...')
+	print('Done.\nSleeping for 15 minutes...', end='', flush=True)
+
 	time.sleep(15*60)
 	print('Done.')
 
 	print('Collecting data for wuhanvirus... ', end='', flush=True)
 	coronavirus.generateData('wuhanvirus', fileCount)
-	print('Done.\nSleeping for 15 minutes...')
+	print('Done.\nSleeping for 15 minutes...', end='', flush=True)
+
 	time.sleep(15*60)
 	print('Done.')
 
 	print('Collecting data for covid19... ', end='', flush=True)
 	coronavirus.generateData('covid19', fileCount)
-	print('Done.\nSleeping for 15 minutes...')
+	print('Done.\nSleeping for 15 minutes...', end='', flush=True)
+
 	time.sleep(15*60)
 	print('Done.')
 	
 	print('Collecting data for COVID2019... ', end='', flush=True)
 	coronavirus.generateData('COVID2019', fileCount)
-	print('Done.\nSleeping for 15 minutes...')
+	print('Done.\nSleeping for 15 minutes...', end='', flush=True)
+
 	time.sleep(15*60)
 	print('Done.')
 	
 	print('Collecting data for CoronaVirusOutbreak... ', end='', flush=True)
 	coronavirus.generateData('CoronaVirusOutbreak', fileCount)
-	print('Done.\nSleeping for 15 minutes... ')
+	print('Done.\nSleeping for 15 minutes... ', end='', flush=True)
+
 	time.sleep(15*60)
 	print('Done.')
 	
