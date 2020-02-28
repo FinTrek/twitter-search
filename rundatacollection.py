7d77b4ca6f87960bba33a75fe2e624d56e6e401b
dannovikov <36117344+dannovikov@users.noreply.github.com>
2020-02-28 01:39:12 -0500

    formatting

--- rundatacollection.py
+++ rundatacollection.py
@@ -18,19 +18,21 @@ while True:
 	coronavirus.generateData('covid19', fileCount)
 	print('Done.\nSleeping for 15 minutes...')
 	time.sleep(15*60)
+	
 	print('Collecting data for COVID2019... ', end='')
 	coronavirus.generateData('COVID2019', fileCount)
 	print('Done.\nSleeping for 15 minutes...')
 	time.sleep(15*60)
+	
 	print('Collecting data for CoronaVirusOutbreak... ', end='')
 	coronavirus.generateData('CoronaVirusOutbreak', fileCount)
 	print('Done.\nSleeping for 15 minutes...')
 	time.sleep(15*60)
+	
 	print('Collecting data for CoronaVirusUpdate... ', end='')
 	coronavirus.generateData('CoronaVirusUpdate', fileCount)
-	print('Done.\nWaiting for 6 hours before collecting again...')
-	time.sleep(15*60)
+	print('Done.\nWaiting for 6 hours before collecting again...', end='')
 	time.sleep(6*60*60)
-
+	print('Done.')
 
 #Every 6 hours, this program generates a tweet dataset
